3cbcbb5466810f044bc3fdcac96d01bfcb5c72d3
James Shaw <js102@zepler.net>
2018-01-29 16:56:13 +0000

    use highOrderFit

--- deformationPlane/run.sh
+++ deformationPlane/run.sh
@@ -6,4 +6,4 @@ createGhostMesh 3
 stitchMesh -perfect -overwrite -region ghostMesh inlet outlet2
 stitchMesh -perfect -overwrite -region ghostMesh outlet inlet1
 setGaussians setGaussiansDict
-scalarDeformationWithGhosts explicit
+scalarDeformationHighOrderFit
