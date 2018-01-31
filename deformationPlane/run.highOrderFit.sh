11da10a974e34a0965c600d3b1ca7c02ae30972b
James Shaw <js102@zepler.net>
2018-01-31 08:32:33 +0000

    add higher-res result for highOrderFit, better than 2nd order!

--- deformationPlane/run.highOrderFit.sh
+++ deformationPlane/run.highOrderFit.sh
@@ -4,6 +4,9 @@ set -e
 rm -rf constant
 rm -rf 1 2 3 4 5
 rm -f 0/U 0/Uf 0/T 0/ghostMesh/TGhost
+
+cp system/controlDict.highOrderFit system/controlDict
+cp system/ghostMesh/fvSchemes.highOrderFit system/ghostMesh/fvSchemes
 blockMesh
 mkdir -p 0
 cp init_0/phi 0/
